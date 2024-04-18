97899727e55f1e65b77ae7fa7c2315c467e4886d
sayyed-123 <166358159+sayyed-123@users.noreply.github.com>
2024-04-18 18:01:09 +0530

    Update training-job.py

--- training-job.py
+++ training-job.py
@@ -36,7 +36,8 @@ base_job_name = 'boston-housing-model'
 
 # Define estimator object
 boston_estimator = Estimator(
-    image_uri=f'{ACCOUNT_ID}.dkr.ecr.{REGION}.amazonaws.com/my-app:latest',
+    #image_uri=f'{ACCOUNT_ID}.dkr.ecr.{REGION}.amazonaws.com/my-app:latest',
+    image_uri="440896245143.dkr.ecr.us-west-2.amazonaws.com/sagemaker-ecr-repo:latest"
     role=IAM_ROLE_NAME ,
     instance_count=1,
     instance_type=training_instance,
